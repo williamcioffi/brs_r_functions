742d14458f835a5af945cf803a5fa3a71b1c5762
williamcioffi <william.cioffi@gmail.com>
2017-12-19 19:02:08 -0500

    made the outer loop the parallel one

--- compare_dives/checkrandomoverlap_foreach.R
+++ compare_dives/checkrandomoverlap_foreach.R
@@ -1,6 +1,9 @@
 # library(doMC)
 # registerDoMC(cores = 4)
 
+beh.backup <- beh
+beh <- beh[grep("ZcTag", beh$DeployID), ]
+beh <- split(beh, beh$DeployID)
 
 ### only need to do this on windows because it deosn't know what an unambiguous format is
 # beh <- lapply(beh, function(l) {
@@ -19,34 +22,38 @@ tagpairs <- t(combn(1:ntags, 2))
 
 starts <- Sys.time()
 outs <- list()
+nsim <- 1000
 
-for(i in 1:ntagpairs) {
-b1 <- beh[[tagpairs[i, 1]]]
-b2 <- beh[[tagpairs[i, 2]]]
 clusterExport(cl, ls())
+	
+outs <- foreach(i = 1:ntagpairs, .combine = c) %dopar% {
+	b1 <- beh[[tagpairs[i, 1]]]
+	b2 <- beh[[tagpairs[i, 2]]]
 
-outs[[i]] <- foreach(n = 1:100, .combine = c) %dopar% {
-	n1 <- build_null_diver_vectorized(beh[[tagpairs[1, 1]]], deployid = "n1")
-	n2 <- build_null_diver_vectorized(beh[[tagpairs[1, 2]]], deployid = "n2")
-	com <- compare_dives(n1, n2)
-	tdif <- abs(com$diff_times)
-	sorl <- vector(mod = "logical", length = length(tdif))
-	sorl[which(tdif <= 60)] <- TRUE
-	sorl[which(tdif >  60)] <- FALSE
-	
-	n <- 4
-	st <- seq(1, length(sorl) - n - 1)
-	en <- seq(n, length(sorl))
-	
-	checks <- list()
-	
-	for(i in 1:(length(sorl) - n - 1)) {
-		checks[[i]] <- sorl[st[i]:en[i]]
+	outchecks <- vector(mode = "numeric", length = nsim)
+	for(sim in 1: nsim) {
+		n1 <- build_null_diver_vectorized(b1, deployid = "n1")
+		n2 <- build_null_diver_vectorized(b2, deployid = "n2")
+		com <- compare_dives(n1, n2)
+		tdif <- abs(com$diff_times)
+		sorl <- vector(mod = "logical", length = length(tdif))
+		sorl[which(tdif <= 60)] <- TRUE
+		sorl[which(tdif >  60)] <- FALSE
+		
+		n <- 4
+		st <- seq(1, length(sorl) - n - 1)
+		en <- seq(n, length(sorl))
+		
+		checks <- list()
+		
+		for(p in 1:(length(sorl) - n - 1)) {
+			checks[[p]] <- sorl[st[p]:en[p]]
+		}
+		
+		outchecks[sim] <- length(which(sapply(checks, all)))
 	}
 	
-	length(which(sapply(checks, all)))
+	list(outchecks)
 }
-}
-t1 <- Sys.time() - starts
 
 stopCluster(cl)
