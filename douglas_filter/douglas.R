cd0f7c1f3383eeb81df9b175119f571476899258
William Cioffi <cioffi@acutorostrata.local>
2018-01-04 01:39:27 -0500

    added beta douglas filter R implementation

---
+++ douglas_filter/douglas.R
@@ -0,0 +1,33 @@
+# douglas filter r implementation
+
+### constants required by MRD DAR and HYB
+MAXREDUN = 10
+KEEP_LC = 1
+
+
+### NOT CURRENTLY USING THESE
+### constants optional for MRD
+KEEPLAST = FALSE
+SKIPLOC = FALSE
+
+### constants required for DAR
+MINRATE = 50
+RATECOEF = 25
+R_ONLY = FALSE
+
+### NOT CURRENTLY USING THESE
+### constants required by HYB
+XMIGRATE = 2
+XOVERRUN = 1.5
+XDIRECT = 20
+XANGLE = 150
+XPERCENT = 20
+TESTP_0A = 2
+TESTP_BZ = 3
+
+### NOT CURRENTLY USING THESE
+### constants for best of day subset
+PICKDAY = FALSE
+MINOFFH = 8
+RANKMETH = 1
+
---
+++ douglas_filter/douglasfunctions.R
@@ -0,0 +1,127 @@
+# douglas filter r implementation functions
+
+#load data
+datadir <- "~/Desktop/CURRENT/nulltest"
+source("~/git/brs_r_functions/basic_tag_functions/load.R")
+streams <- loadtag(datadir)
+argos <- streams$argos
+argos <- argos[which(argos$LocationQuality %in% c("0", "1", "2", "3", "A", "B", "Z")), ]
+
+movedat <- data.frame(
+	t = argos$Date,
+	lat = argos$Latitude,
+	lon = argos$Longitude,
+	lc = argos$LocationQuality
+)
+
+movedat <- split(movedat, argos$DeployID)
+movedat <- movedat[[1]]
+
+movedat$lc <- factor(movedat$lc, levels = c("Z", "B", "A", "0", "1", "2", "3"))
+
+require(geosphere)
+
+### three iterations of this
+for(i in 1:3) {
+	retain <- rep(FALSE, nrow(movedat))
+	retain[1] <- TRUE # first one
+	retain[length(retain)] <- TRUE # last one
+	
+	for(n in 1:(nrow(movedat) - 2)) {		
+		iA <- n
+		iB <- iA + 1
+		iC <- iB + 1
+		
+		A <- c(movedat$lon[iA], movedat$lat[iA])
+		B <- c(movedat$lon[iB], movedat$lat[iB])
+		C <- c(movedat$lon[iC], movedat$lat[iC])
+		
+		dars <- calcDAR(A, B, C)
+		
+		### first three
+		if(dars$dist1 < MAXREDUN) {
+			retain_tmp <- TRUE	
+		} else if(as.numeric(movedat$lc[iB]) >= KEEP_LC + 4) { #fix this so they appear in the right order
+			retain_tmp <- TRUE
+		} else if(dars$alpha < -25 + RATECOEF*log(min(dars$dist1, dars$dist2))) {
+			retain_tmp <- FALSE
+		} else if(dars$rate1 > MINRATE) {
+			retain_tmp <- FALSE
+		} else if(dars$rate2 > MINRATE | dist1 + dist5 > dist4 + dist3) {
+			retain_tmp <- FALSE
+		} else {
+			retain_tmp <- TRUE
+		}
+		
+		retain[iB] <- retain_tmp
+		
+		# plot(movedat$lon[iA:iD], movedat$lat[iA:iD], main = paste(retain_tmp))
+		# points(movedat$lon[iA:iD], movedat$lat[iA:iD], col = "red")
+		# lines(movedat$lon[iA:iD], movedat$lat[iA:iD], lty = 2)
+		# text(movedat$lon[iA:iD], movedat$lat[iA:iD], LETTERS[1:4], pos = 2)
+		# readline(prompt="Press [enter] to continue")
+	}
+	
+	movedat <- movedat[retain, ]
+}
+
+# two iterations of this
+
+for(i in 1:2) {
+	retain <- rep(FALSE, nrow(movedat))
+	retain[1] <- TRUE # first one
+	retain[length(retain)] <- TRUE # last one
+	
+	for(n in 1:(nrow(movedat) - 2)) {		
+		iA <- n
+		iB <- iA + 1
+		iC <- iB + 1
+		
+		A <- c(movedat$lon[iA], movedat$lat[iA])
+		B <- c(movedat$lon[iB], movedat$lat[iB])
+		C <- c(movedat$lon[iC], movedat$lat[iC])
+		
+		dars <- calcDAR(A, B, C)
+		
+		### first three
+		if(dars$dist1 < MAXREDUN) {
+			retain_tmp <- TRUE	
+		} else if(as.numeric(movedat$lc[iB]) >= KEEP_LC + 4) { #fix this so they appear in the right order
+			retain_tmp <- TRUE
+		} else if(dars$alpha < -25 + RATECOEF*log(min(dars$dist1, dars$dist2))) {
+			retain_tmp <- FALSE
+		} else if(dars$rate1 > MINRATE | dars$rate2 > MINRATE) {
+			retain_tmp <- FALSE
+		} else {
+			retain_tmp <- TRUE
+		}
+				
+		retain[iB] <- retain_tmp
+	}
+	
+	movedat <- movedat[retain, ]
+}
+
+
+calcDAR <- function(A, B, C) {
+	dist1 <- distGeo(A, B) / 1000
+	dist2 <- distGeo(B, C) / 1000
+	
+	time1 <- as.numeric(difftime(movedat$t[iB], movedat$t[iA], units = "hours"))
+	time2 <- as.numeric(difftime(movedat$t[iC], movedat$t[iB], units = "hours"))
+	rate1 <- dist1 / time1
+	rate2 <- dist2 / time2
+	
+	# alpha #how do I calculate alpha?
+	a1 <- bearing(A, B)
+	a2 <- bearing(B, C)
+	
+	aa1 <- (a1 + 360) %% 360
+	aa2 <- (a2 + 360) %% 360
+	
+	deltaz <- abs(aa1 - aa2)
+	alpha <- abs(180 - deltaz)
+	
+	list(dist1 = dist1, dist2 = dist2, rate1 = rate1, rate2 = rate2, alpha = alpha)
+}
+
