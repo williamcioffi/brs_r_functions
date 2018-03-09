a6540d06e71f1f883602e866217958db4aa3ba5e
williamcioffi <william.cioffi@gmail.com>
2018-03-08 20:24:45 -0500

    fixed horrific error

--- compare_dives/compare_dives_new.R
+++ compare_dives/compare_dives_new.R
@@ -15,77 +15,84 @@ compare_dives <- function(b1, b2) {
 	b1 <- b1[which(b1$Start >= start & b1$End <= end), ]
 	b2 <- b2[which(b2$Start >= start & b2$End <= end), ]
 	
-	# take the midpoint of duration and depth
-	b1[, 'DurationMean'] <- apply(b1[, c('DurationMax', 'DurationMin')], 1, mean)
-	b2[, 'DurationMean'] <- apply(b2[, c('DurationMax', 'DurationMin')], 1, mean)
-
-	b1[, 'DepthMean'] <- apply(b1[, c('DepthMax', 'DepthMin')], 1, mean)
-	b2[, 'DepthMean'] <- apply(b2[, c('DepthMax', 'DepthMin')], 1, mean)
+	if(nrow(b1) > 0 & nrow(b2) > 0) {
+		# take the midpoint of duration and depth
+		b1[, 'DurationMean'] <- apply(b1[, c('DurationMax', 'DurationMin')], 1, mean)
+		b2[, 'DurationMean'] <- apply(b2[, c('DurationMax', 'DurationMin')], 1, mean)
 	
-	# remove messages
-	b1 <- b1[b1$What != "Message", ]
-	b2 <- b2[b2$What != "Message", ]
-
-	# grab end times and make sure they are in posix
-	b1_ens <- as.POSIXct(b1$End, tz = "UTC")
-	b2_ens <- as.POSIXct(b2$End, tz = "UTC")
+		b1[, 'DepthMean'] <- apply(b1[, c('DepthMax', 'DepthMin')], 1, mean)
+		b2[, 'DepthMean'] <- apply(b2[, c('DepthMax', 'DepthMin')], 1, mean)
+		
+		# remove messages
+		b1 <- b1[b1$What != "Message", ]
+		b2 <- b2[b2$What != "Message", ]
 	
-	# delete any gaps
-	gaps1 <- findgaps(b1)
-	gaps2 <- findgaps(b2)
-	
-	if(gaps1$ngaps > 0) {
-		for(i in 1:gaps1$ngaps) {
-			b2_ens[b2_ens > gaps1$gap_st[i] & b2_ens < gaps1$gap_en[i]] <- NA
+		# grab end times and make sure they are in posix
+		b1_ens <- as.POSIXct(b1$End, tz = "UTC")
+		b2_ens <- as.POSIXct(b2$End, tz = "UTC")
+		
+		# delete any gaps
+		gaps1 <- findgaps(b1)
+		gaps2 <- findgaps(b2)
+		
+		if(gaps1$ngaps > 0) {
+			for(i in 1:gaps1$ngaps) {
+				b2_ens[b2_ens > gaps1$gap_st[i] & b2_ens < gaps1$gap_en[i]] <- NA
+			}
+			b2 <- b2[!is.na(b2_ens), ]
+			b2_ens <- b2_ens[!is.na(b2_ens)]
 		}
-		b2 <- b2[!is.na(b2_ens), ]
-		b2_ens <- b2_ens[!is.na(b2_ens)]
-	}
-	
-	if(gaps2$ngaps > 0) {
-		for(i in 1:gaps2$ngaps) {
-			b1_ens[b1_ens > gaps2$gap_st[i] & b1_ens < gaps2$gap_en[i]] <- NA
+		
+		if(gaps2$ngaps > 0) {
+			for(i in 1:gaps2$ngaps) {
+				b1_ens[b1_ens > gaps2$gap_st[i] & b1_ens < gaps2$gap_en[i]] <- NA
+			}
+			b1 <- b1[!is.na(b1_ens), ]
+			b1_ens <- b1_ens[!is.na(b1_ens)]
 		}
-		b1 <- b1[!is.na(b1_ens), ]
-		b1_ens <- b1_ens[!is.na(b1_ens)]
+		
+		# if b2 is longer than b1 flip them
+		if(nrow(b2) > nrow(b1)) {
+			tmpb1 <- b1
+			b1 <- b2
+			b2 <- tmpb1
+			
+			tmpb1ens <- b1_ens
+			b1_ens <- b2_ens
+			b2_ens <- tmpb1ens
+		}
+		
+		# set up some vectors to hold the results
+		t1 <- vector()
+		t2 <- vector()
+		diff_times <- vector()
+		diff_durs <- vector()
+		diff_deps <- vector()
+		diff_b1_type <- vector()
+		diff_b2_type <- vector()
+		
+		for(i in 1:length(b1_ens)) {
+			dt <- abs(difftime(b1_ens[i], b2_ens, units = "secs"))
+			dis <- which.min(dt)
+			
+			t1[i] <- as.character(b1_ens[i])
+			t2[i] <- as.character(b2_ens[dis])
+			
+			diff_times[i] <- difftime(b1_ens[i], b2_ens[dis], units = "secs")
+			diff_durs[i] <- b1$DurationMean[i] - b2$DurationMean[dis]
+			diff_deps[i] <- b1$DepthMean[i] - b2$DepthMean[dis]
+			diff_b1_type[i] <- b1$What[i]
+			diff_b2_type[i] <- b2$What[dis]
+		}
+		
+		t1 <- as.POSIXct(t1, tz = "UTC")
+		t2 <- as.POSIXct(t2, tz = "UTC")
+		
+		out <- list(tag1 = b1$DeployID[1], tag2 = b2$DeployID[2], t1 = t1, t2_matched = t2, diff_times = diff_times, diff_durs = diff_durs, diff_deps = diff_deps, type1 = diff_b1_type, type2 = diff_b2_type)
+	} else {
+		warning("maybe tags don't overlap?")
+		out <- NULL
 	}
 	
-	# if b2 is longer than b1 flip them
-	if(nrow(b2) > nrow(b1)) {
-		tmpb1 <- b1
-		b1 <- b2
-		b2 <- tmpb1
-		
-		tmpb1ens <- b1_ens
-		b1_ens <- b2_ens
-		b2_ens <- tmpb1ens
-	}
-	
-	# set up some vectors to hold the results
-	t1 <- vector()
-	t2 <- vector()
-	diff_times <- vector()
-	diff_durs <- vector()
-	diff_deps <- vector()
-	diff_b1_type <- vector()
-	diff_b2_type <- vector()
-	
-	for(i in 1:length(b1_ens)) {
-		dt <- abs(difftime(b1_ens[i], b2_ens, units = "secs"))
-		dis <- which.min(dt)
-		
-		t1[i] <- as.character(b1_ens[i])
-		t2[i] <- as.character(b2_ens[dis])
-		
-		diff_times[i] <- difftime(b1_ens[i], b2_ens[dis], units = "secs")
-		diff_durs[i] <- b1$DurationMean[i] - b2$DurationMean[dis]
-		diff_deps[i] <- b1$DepthMean[i] - b2$DepthMean[dis]
-		diff_b1_type[i] <- b1$What[i]
-		diff_b2_type[i] <- b2$What[dis]
-	}
-	
-	t1 <- as.POSIXct(t1, tz = "UTC")
-	t2 <- as.POSIXct(t2, tz = "UTC")
-	
-	list(tag1 = b1$DeployID[1], tag2 = b2$DeployID[2], t1 = t1, t2_matched = t2, diff_times = diff_times, diff_durs = diff_durs, diff_deps = diff_deps, type1 = diff_b1_type, type2 = diff_b2_type)
+	out
 }
